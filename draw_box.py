fad3f6bb0fb568daf1b44db50729fc9dfad76c4d
Mushfik <ghostmushfik@gmail.com>
2020-02-24 12:30:55 -0700

    Test

--- draw_box.py
+++ draw_box.py
@@ -1,9 +1,20 @@
+import base64
+import numpy as np
+from io import BytesIO
+
 import face_recognition.face_recognition as fr
 from PIL import Image, ImageDraw
 
-def drawRectangle(image):
+def drawRectangle(image, known_encodings):
+    status = False
     # Find all the faces and face encodings in the unknown image
     face_locations = fr.face_locations(image)
+
+    known_face_names = [
+        "Michael Reeves",
+        "Michael Reeves"
+    ]
+
     face_encodings = fr.face_encodings(image, face_locations)
 
     # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
@@ -12,13 +23,38 @@ def drawRectangle(image):
     # Create a Pillow ImageDraw Draw instance to draw with
     draw = ImageDraw.Draw(pil_image)
 
-    # Loop through each face found in the unknown image
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # Draw a box around the face using the Pillow module
-        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+        # See if the face is a match for the known face(s)
+        matches = fr.compare_faces(known_encodings, face_encoding)
+
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = fr.face_distance(known_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+            # Draw a label with a name below the face
+            pad = 10
+            text_width, text_height = draw.textsize(name)
+            draw.rectangle(((left-pad, bottom - text_height - 10), (right+pad, bottom+pad)), fill=(255, 0, 0), outline=(255, 0, 0))
+            draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+            # Draw a box around the face using the Pillow module
+            draw.rectangle(((left-pad, top-pad), (right+pad, bottom+pad)), outline=(255, 0, 0), width=3)
+            status = True
+
     del draw
 
-    pil_image.show()
+    buffered = BytesIO()
+    pil_image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+    return [status, img_str]
 
     # You can also save a copy of the new image to disk if you want by uncommenting this line
     # pil_image.save("image_with_boxes.jpg")
--- ss_scraper.py
+++ ss_scraper.py
@@ -3,53 +3,24 @@ import face_recognition.face_recognition as fr
 from selenium import webdriver
 import draw_box
 import os
+import requests
 from selenium.webdriver.chrome.options import Options
 import time
 
-dir_name = 'michael'
-
-image_dir = os.listdir(dir_name)
-known_images = [fr.load_image_file(dir_name+'/'+i) for i in image_dir if i.endswith('.jpg')]
-known_encodings = []
-
-for image in known_images:
-    temp = fr.face_encodings(image)
-    if len(temp) > 0:
-        known_encodings.append(temp[0])
+DEBUG = True
+GLOBAL_STATE = False
 
 
-chrome_options = Options()
-chrome_options.add_argument("--window-size=1920,1080")
-
-js = open("set_res.js")
-
-def isPersonThere(im):
-    arg_encoding_arr = fr.face_encodings(im)
-    if len(arg_encoding_arr) > 0:
-        arg_encoding = arg_encoding_arr[0]
-    else:
-        return False
-
-    # result = fr.compare_faces([known_encoding], arg_encoding)
-    results = fr.compare_faces(known_encodings, arg_encoding)
-    return True in results
-
-
-def scrapFromTwitch(url, fn):
+def scrapFromTwitch(url, fn, chrome_options, channel, known_encodings):
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get(url)
-
-    # el = driver.find_element_by_css_selector('button[data-a-target="player-settings-button"]')
-    # el.click()
-    # el = driver.find_element_by_css_selector('button[data-a-target="player-settings-menu-item-quality"]')
-    # el.click()
-    # el = driver.find_element_by_css_selector('div[data-a-target="player-settings-submenu-quality-option"] input:first-child')
-    # el.click()
+    js = open("set_res.js")
     driver.execute_script(js.read())
 
-    i = 0
-    while i < 10:
-        time.sleep(10)
+    # i = 0
+    while isLive(channel) or DEBUG:
+
+        time.sleep(5)
         ss_script = open("take_ss.js")
         driver.execute_script(ss_script.read())
         dl = driver.find_element_by_id('selenium-download-link')
@@ -59,16 +30,68 @@ def scrapFromTwitch(url, fn):
         with open(fn, 'wb') as f:
             f.write(base64.b64decode(image64))
 
-        print(isPersonThere(fr.load_image_file(fn)))
-        draw_box.drawRectangle(fr.load_image_file(fn))
+        result = draw_box.drawRectangle(fr.load_image_file(fn), known_encodings)
+        print(result[0])
+        sendData(result[0], result[1], channel)
         # os.remove(filename)
-        i += 1
+        # i += 1
 
     driver.close()
+    sendEndofStream()
 
 
-filename = 'ss.jpg'
-link = "https://www.twitch.tv/videos/556436677?t=5h20m40s"
-# link = "https://www.twitch.tv/lilypichu"
-scrapFromTwitch(link, filename)
+def sendData(status, image, channel):
+    url = 'http://cracksignal.scienceontheweb.net/api/admin/update.php/'
+    if DEBUG:
+        url = 'http://192.168.0.13/crackSignal/api/admin/update.php/'
+    data = {'auth': 'qRE*S6Trzbwb5Vxxvz*gqfqd@%pEtwCA', 'status': status, 'image': image, 'channel': channel}
+    r = requests.post(url=url, data=data)
 
+
+def sendEndofStream():
+    url = 'http://cracksignal.scienceontheweb.net/api/admin/update.php/'
+    if DEBUG:
+        url = 'http://192.168.0.13/crackSignal/api/admin/update.php/'
+    data = {'auth': 'qRE*S6Trzbwb5Vxxvz*gqfqd@%pEtwCA', 'status': False, 'image': False, 'channel': False, 'eos': True}
+    r = requests.post(url=url, data=data)
+
+def isLive(channel):
+    url = 'https://api.twitch.tv/helix/streams?user_login=' + channel
+    headers = {'Client-ID': 'tdocwr9sz4pi3odc5emk4qw3jzidqo'}
+    r = requests.get(url, headers=headers)
+
+    try:
+        json = r.json()
+    except:
+        return False
+
+    if len(json['data']) > 0:
+        return True
+    else:
+        return False
+
+
+def startScraping(dir_name, channel):
+    image_dir = os.listdir(dir_name)
+    known_images = [fr.load_image_file(dir_name+'/'+i) for i in image_dir if i.endswith('.jpg')]
+    known_encodings = []
+
+    for image in known_images:
+        temp = fr.face_encodings(image)
+        if len(temp) > 0:
+            known_encodings.append(temp[0])
+
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless")
+
+    filename = 'ss.jpg'
+    link = "https://www.twitch.tv/" + channel
+    if DEBUG:
+        link = "https://www.twitch.tv/videos/556436677?t=2h48m20s"
+    scrapFromTwitch(link, filename, chrome_options, channel, known_encodings)
+
+
+while True:
+    if isLive('lilypichu') or DEBUG:
+        startScraping('michael', 'lilypichu')
